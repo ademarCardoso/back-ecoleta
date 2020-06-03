d7bc62df20b96eb93aa879bf9f76f945cd1c8987
ademarCardoso <ac@storyblok.com>
2020-06-03 19:52:48 -0300

    add more configs to run the database

---
+++ knexfile.ts
@@ -0,0 +1,14 @@
+import path from 'path'
+
+// comando para criar as tables: npx knex migrate:latest --knexfile knexfile.ts migrate:latest
+ 
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'src', 'database', 'databese.sqlite')
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+  },
+  useNullAsDefault: true
+}
